31613e1e776e480213d1e2f8200859a7fcc07fcf
Irham Manthiqo Noor <irhammn0407@gmail.com>
2026-07-06 15:56:45 +0700

    pengambahan User Repo

---
+++ src/main/java/com/imnkeuangan/personal_finance_api2/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.imnkeuangan.personal_finance_api2.repository;
+
+import com.imnkeuangan.personal_finance_api2.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Method tambahan untuk mencari user berdasarkan username (berguna untuk login nanti)
+    Optional<User> findByUsername(String username);
+
+    // Method untuk mengecek apakah email sudah terdaftar atau belum
+    boolean existsByEmail(String email);
+}
